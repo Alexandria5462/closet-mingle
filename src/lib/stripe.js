32acf35538c5505ecbd15275c7456a0cea5c61a3
Alexandria Anderson <alexandriaanderson01@gmail.com>
2026-06-03 17:34:11 -0400

    Update Stripe price IDs

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -2,4 +2,7 @@ export const STRIPE_PUBLISHABLE_KEY = process.env.REACT_APP_STRIPE_PUBLISHABLE_K
 export const PRICES = {
   premium_monthly: "price_1Tb0spR0XolMJ7tnx8yhgOwU",
   pay_per_session: "price_1Tb0yMR0XolMJ7tnLE403aZu",
+  premium_plus: "price_1TeMEvR0XolMJ7tnYpsd5moK",
+  stylist_monthly: "price_1TeMHvR0XolMJ7tnjtiKsTZr",
+ stylist_annual: "price_1TeMK5R0XolMJ7tnWzzYfgXK",
 };
